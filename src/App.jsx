5c49d624e4d9795497c869c5eccdea7c1bb3d7ab
broyles0606 <broyles0606@gmail.com>
2026-03-19 02:27:53 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,6 @@ import React, { useEffect, useMemo, useState } from 'react'
 import { initializeApp, getApps } from 'firebase/app'
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCwJBvcBEqDqNtCJ854LXKNKFnnVwbR4Ro",
   authDomain: "maxfit-panel.firebaseapp.com",
